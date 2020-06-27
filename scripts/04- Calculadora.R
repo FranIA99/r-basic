5519fe6d4dfab7554ba9f194968325fa7cef0078
FranIA99 <franciscoalberto90@hotmail.com>
2020-06-27 17:42:49 -0300

    Update 04- Calculadora.R

--- scripts/04- Calculadora.R
+++ scripts/04- Calculadora.R
@@ -6,3 +6,17 @@
 2*pi
 # siempre escribir pi en R
 2^50#1.1259e+15= 1125900000000000
+sqrt(9)
+log(1000, 10)
+factorial(9)
+choose(3,4)
+log10(1000)
+abs(8)
+exp(2)
+exp(5)
+choose(4,2)
+choose(2,4)
+choose(4,4)
+factorial(5)
+log(3^6, 5)
+factorial(5)+exp(2)-log10(100)
