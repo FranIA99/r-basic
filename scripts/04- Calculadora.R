e3c080e2c728d9626d839685ad9e0abfa9a97438
FranIA99 <franciscoalberto90@hotmail.com>
2020-06-29 18:24:40 -0300

    Update 04- Calculadora.R

--- scripts/04- Calculadora.R
+++ scripts/04- Calculadora.R
@@ -20,3 +20,16 @@ choose(4,4)
 factorial(5)
 log(3^6, 5)
 factorial(5)+exp(2)-log10(100)
+sin(120*pi/180)
+cos(120*pi/180)
+tan(80*pi/180)
+asin(0.5)*180/pi
+acos(0.5)*180/pi
+print(sqrt(2),2)
+round(sqrt(3),2)
+floor(sqrt(3))
+ceiling(sqrt(3))
+round(sqrt(3), 1)
+round(sqrt(3))
+#si queres poner primero los digitos
+round(digits=2, sqrt(2))
