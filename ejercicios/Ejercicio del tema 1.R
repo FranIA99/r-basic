8c11f2810cb9d8d9acdb84ec4695520938768004
FranIA99 <franciscoalberto90@hotmail.com>
2020-07-02 18:07:40 -0300

    Update Ejercicio del tema 1.R

--- ejercicios/Ejercicio del tema 1.R
+++ ejercicios/Ejercicio del tema 1.R
@@ -15,3 +15,18 @@ opbasic= function(a,b){
   
 }
 opbasic(5,4)
+
+#Numeros complejos
+(3+5i) * (4+3i)
+3+5i-2+4i
+(3+6i) /(4-6i)
+complex(real=3, imaginary=sqrt(3))
+cos(2+3i)
+sqrt(3+2i)
+sin(3+2i)
+z1= 2+3i
+Mod(z1)
+Arg(z1)
+Re(z1)
+Im(z1)
+Conj(z1)
