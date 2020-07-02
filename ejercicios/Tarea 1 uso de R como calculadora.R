d81f14b59598f8d2e17a9118a40e2faa58478e0a
FranIA99 <franciscoalberto90@hotmail.com>
2020-07-02 20:44:18 -0300

    Update Tarea 1 uso de R como calculadora.R

---
+++ ejercicios/Tarea 1 uso de R como calculadora.R
@@ -0,0 +1,13 @@
+z= 3*exp(1)-pi
+round(z,3)
+
+r=(2+3i)^ 2/(5+8i)
+Mod(r)
+round(Mod(r), 3)
+
+ecuacion=function(a,b){ #ax+b=0 x=b/a
+  b/a
+}
+ecuacion(5,3)
+ecuacion(7,-14) # 7x+4=18 7x-14=0
+ecuacion(1,0) # x+1=1 x+0=0
